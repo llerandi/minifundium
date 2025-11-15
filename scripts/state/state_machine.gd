00b5a2da51729aeb72b51efc5a5365ebd74b9318
Edward <llerandi@users.noreply.github.com>
2025-11-15 22:09:01 +0100

    chore(script): to transition from one state to another

--- scripts/state/state_machine.gd
+++ scripts/state/state_machine.gd
@@ -16,10 +16,18 @@ class_name StateMachine extends Node
 var states: Dictionary = {}
 var current_state: State
 var current_state_name: String
+var parent_name: String
 
 # Called when the node enters the scene tree for the first time
 ## Handles the initial setup of the state machine
 func _ready() -> void:
+	parent_name = get_parent().name
+	
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.transition.connect(transition_to)
+	
 	if initial_state:
 		initial_state._on_enter()
 		current_state = initial_state
@@ -38,5 +46,19 @@ func _physics_process(delta: float) -> void:
 		current_state._on_next_transitions()
 
 # Handles the logic of switching from one state to another
-func transition_to() -> void:
-	pass # I've only 1 state right now
+func transition_to(state_name: String) -> void:
+	if state_name == current_state.name.to_lower():
+		return
+	
+	var new_state = states.get(state_name.to_lower())
+	
+	if !new_state:
+		return
+	
+	if current_state:
+		current_state._on_exit()
+	
+	new_state._on_enter()
+	
+	current_state = new_state
+	current_state_name = current_state_name.to_lower()
