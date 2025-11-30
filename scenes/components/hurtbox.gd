860749368803fa95087df2f38a573aed7a15db7b
Edward <llerandi@users.noreply.github.com>
2025-11-30 15:28:03 +0100

    refactor(script): better readable logic

--- scenes/components/hurtbox.gd
+++ scenes/components/hurtbox.gd
@@ -1,14 +1,20 @@
 class_name Hurtbox extends Area2D
 
-@export var current_tool: DataTypes.Tool = DataTypes.Tool.NONE
+@export var tool: DataTypes.Tool = DataTypes.Tool.NONE
 
 # Signals allow all connected Callables to listen and react to events, without directly referencing one another
 ## https://docs.godotengine.org/en/4.4/classes/class_signal.html
-signal on_hurt
+signal struck
 
 func _on_area_entered(area: Area2D) -> void:
-	# It checks if the area that entered is actually a Hitbox
 	var hitbox = area as Hitbox
 	
-	if current_tool == hitbox.current_tool:
-		on_hurt.emit(hitbox.damage)
+	# It checks if the area that entered is actually a Hitbox
+	if not hitbox:
+		return
+	
+	# If the tool doesn't match
+	if tool != hitbox.current_tool:
+		return
+	
+	struck.emit(hitbox.damage)
