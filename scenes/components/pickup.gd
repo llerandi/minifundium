9debd71ecef16af28d51cdd1a5950db6d5d01790
Edward <llerandi@users.noreply.github.com>
2025-12-02 20:02:26 +0100

    feat: entering the area of the pickup object event

---
+++ scenes/components/pickup.gd
@@ -0,0 +1,8 @@
+class_name Pickup extends Area2D
+
+@export var item_pickup: String
+
+func _on_body_entered(body: Node2D) -> void:
+	# Check if the player is on the item area for picking up
+	if entity is Player:
+		get_parent().queue_free() # It will be added on another scene then get_parent() will work
