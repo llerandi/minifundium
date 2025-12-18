66f17beb0629dcc26c0f75ef1101683f584b8508
Edward <llerandi@users.noreply.github.com>
2025-12-18 16:14:12 +0100

    feat(script): load the sapling scene after chopping down a tree along the log scene

--- scenes/objects/environment/trees/tree.gd
+++ scenes/objects/environment/trees/tree.gd
@@ -5,6 +5,8 @@ extends Sprite2D
 
 # To get the scene for the log after chopping down the tree
 var log_scene_path = preload("res://scenes/objects/environment/trees/log.tscn")
+# To get the scene for the sapling after chopping down the tree
+var sapling_scene_path = preload("res://scenes/objects/environment/trees/sapling.tscn")
 
 func _ready() -> void:
 	hurtbox.struck.connect(on_hurt)
@@ -16,6 +18,7 @@ func on_hurt(hit: int) -> void:
 
 func on_health_depleted() -> void:
 	call_deferred("log_scene_load") # Instantiate the log scene on the next frame
+	call_deferred("sapling_scene_load") # Instantiate the sapling scene on the next frame
 	
 	queue_free()
 
@@ -23,3 +26,8 @@ func log_scene_load() -> void:
 	var log_init = log_scene_path.instantiate() as Node2D # Instantiate and cast as Node2D
 	log_init.global_position = global_position
 	get_parent().add_child(log_init) # Adding the log scene as Node2D to the parent (so here)
+
+func sapling_scene_load() -> void:
+	var sapling_init = sapling_scene_path.instantiate() as Node2D # Instantiate and cast as Node2D
+	sapling_init.global_position = global_position
+	get_parent().add_child(sapling_init) # Adding the sapling scene as Node2D to the parent (so here)
