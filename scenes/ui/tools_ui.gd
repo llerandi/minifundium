3ed7754fc175f5c7f4bb87b0f5f7fe77aac1f9f7
Edward <llerandi@users.noreply.github.com>
2025-12-26 17:07:20 +0100

    chore(script): refactor in the No Tool button for better cohesion in the code

--- scenes/ui/tools_ui.gd
+++ scenes/ui/tools_ui.gd
@@ -1,10 +1,11 @@
 extends PanelContainer
 
+@onready var no_tool: Button = $MarginContainer/HBoxContainer/NoTool
 @onready var watering_can: Button = $MarginContainer/HBoxContainer/WateringCan
 @onready var axe: Button = $MarginContainer/HBoxContainer/Axe
 @onready var hoe: Button = $MarginContainer/HBoxContainer/Hoe
 
-func _on_none_pressed() -> void:
+func _on_no_tool_pressed() -> void:
 	CurrentTool.selecting_tool(DataTypes.Tool.NONE)
 
 func _on_watering_can_pressed() -> void:
