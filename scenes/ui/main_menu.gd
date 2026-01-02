3c69147d6e5cb88c13544470008872efd3493d6e
Edward <llerandi@users.noreply.github.com>
2026-01-02 19:07:56 +0100

    feat(script): script for handling the main menu buttons

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_save_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.
