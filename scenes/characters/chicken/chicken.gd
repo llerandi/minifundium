c55940fee6ac588fa37abe8ffec7c05acf21dd5e
Edward <llerandi@users.noreply.github.com>
2026-05-09 13:07:52 +0200

    feat(chicken): input key handler and command for lab1

--- scenes/characters/chicken/chicken.gd
+++ scenes/characters/chicken/chicken.gd
@@ -2,6 +2,20 @@ class_name Chicken extends CharacterBody2D
 
 @onready var state: StateMachine = $StateMachine
 var target_crop: Node2D = null
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		command_harvest()
+
+func command_harvest() -> void:
+	target_crop = find_ready_crop()
+	
+	if target_crop != null:
+		print("A crop ready for harvest was found. On the way to harvest it...")
+		state.transition_to("Movement")
+	else:
+		print("No crops ready for harvest were found...")
+		
 func find_ready_crop() -> Node2D:
 	# Searching for the node called Crops in the actual scene, here all the crops will be listed within
 	var crops_node = get_tree().current_scene.find_child("Crops", true, false)
