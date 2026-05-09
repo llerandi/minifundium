634a1a1fe1121ef7a86900065f53e612abcff627
Edward <llerandi@users.noreply.github.com>
2026-05-09 14:17:56 +0200

    feat(harvest): on_enter function setup

--- scenes/characters/chicken/harvest.gd
+++ scenes/characters/chicken/harvest.gd
@@ -2,3 +2,16 @@ extends State
 
 @export var chicken: Chicken
 @export var sprite: AnimatedSprite2D
+
+func _on_enter() -> void:
+	sprite.play("down")
+	
+	await get_tree().create_timer(1.5).timeout
+	
+	if chicken.target_crop != null and is_instance_valid(chicken.target_crop):
+		chicken.target_crop.on_harvest()
+	
+	chicken.target_crop = null
+	
+	transition.emit("Idle")
+
