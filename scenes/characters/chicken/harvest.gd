cf1596062db2dcc8e596de0420c960d212e4adcd
Edward <llerandi@users.noreply.github.com>
2026-05-10 14:56:42 +0200

    fix(harvest): refactor the whole method behaviour to harvest using pickup node instead growth state == crop

--- scenes/characters/chicken/harvest.gd
+++ scenes/characters/chicken/harvest.gd
@@ -9,15 +9,20 @@ func _on_enter() -> void:
 	await get_tree().create_timer(1.5).timeout
 	
 	if chicken.target_crop != null and is_instance_valid(chicken.target_crop):
-		chicken.target_crop.on_harvest()
+		#chicken.target_crop.on_harvest()
 		
 		# To get the position
-		var target_position: Vector2 = chicken.target_crop.global_position
+		#var target_position: Vector2 = chicken.target_crop.global_position
 		# To get the facing direction
-		var target_direction: Vector2 = chicken.global_position.direction_to(target_position)
+		#var target_direction: Vector2 = chicken.global_position.direction_to(target_position)
+		var pickup_node = chicken.target_crop.get_node("Pickup")
+		if pickup_node:
+			Resources.add_resource(pickup_node.item_pickup)
+			chicken.target_crop.queue_free()
 	
 	chicken.target_crop = null
 	
+	#chicken.request_next_action("Idle")
 	transition.emit("Idle")
 
 func _on_exit() -> void:
