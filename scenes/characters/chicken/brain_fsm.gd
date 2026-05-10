47e7051dac9d6cb7303b8757d6b4cbc83a617f28
Edward <llerandi@users.noreply.github.com>
2026-05-10 12:16:24 +0200

    feat(script): init brain

--- scenes/characters/chicken/brain_fsm.gd
+++ scenes/characters/chicken/brain_fsm.gd
@@ -1,1 +1,5 @@
-extends Node
+class_name BrainFSM extends Node
+
+var chicken: Chicken
+
+# This brain simply checks to see if there are crops ready to be harvested and the chicken goes to harvest them
