e6b67f5013944b8070f157e1d6194b10679f4ae5
Edward <llerandi@users.noreply.github.com>
2026-05-10 16:20:17 +0200

    feat(main_menu): added the behaviour to transition from main menu to map selector

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,9 +1,9 @@
 extends CanvasLayer
 
-var main_scene_path: String = "res://scenes/test/main_scene.tscn"
+var map_selector_menu_scene_path: String = "res://scenes/ui/map_selector_menu.tscn"
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file(main_scene_path)
+	get_tree().change_scene_to_file(map_selector_menu_scene_path)
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
