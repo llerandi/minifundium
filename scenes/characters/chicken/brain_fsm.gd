fbb4b0eb5b04da9311c7be262371516d5ce72d8f
Edward <llerandi@users.noreply.github.com>
2026-05-10 12:17:19 +0200

    feat(script): brain behaviour after doing a task

--- scenes/characters/chicken/brain_fsm.gd
+++ scenes/characters/chicken/brain_fsm.gd
@@ -6,3 +6,12 @@ var chicken: Chicken
 
 func setup(_chicken: Chicken) -> void:
 	chicken = _chicken
+
+func decide_next_action(current_state: String) -> void:
+	if current_state == "Idle" or current_state == "Harvest":
+		var crop = chicken.find_ready_crop()
+		if crop:
+			chicken.target_crop = crop
+			chicken.state.transition_to("Movement")
+		else:
+			chicken.state.transition_to("Idle")
