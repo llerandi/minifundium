8f65b5461d7ececd90505382e5c7f5827f25ff62
Edward <llerandi@users.noreply.github.com>
2026-05-10 12:17:03 +0200

    feat(script): setup for fsm brain

--- scenes/characters/chicken/brain_fsm.gd
+++ scenes/characters/chicken/brain_fsm.gd
@@ -3,3 +3,6 @@ class_name BrainFSM extends Node
 var chicken: Chicken
 
 # This brain simply checks to see if there are crops ready to be harvested and the chicken goes to harvest them
+
+func setup(_chicken: Chicken) -> void:
+	chicken = _chicken
