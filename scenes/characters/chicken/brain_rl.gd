138ece1242098dde3b75cd3116e85429991ae4b8
Edward <llerandi@users.noreply.github.com>
2026-05-16 10:28:42 +0200

    feat: matrix added

--- scenes/characters/chicken/brain_rl.gd
+++ scenes/characters/chicken/brain_rl.gd
@@ -1,3 +1,11 @@
 class_name BrainRL extends Node
 
 var chicken: Chicken
+
+var last_state: String = "idle" # Saving the last action for the matrix
+
+# Set it to 1 because it should initially be 50%–50%
+var matrix = {
+	"idle": {"idle": 1, "harvest": 1},
+	"harvest": {"idle": 1, "harvest": 1}
+}
