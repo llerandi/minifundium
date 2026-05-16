7ec4f36a0e38a86a5e3bd7553bede1f1aae698d9
Edward <llerandi@users.noreply.github.com>
2026-05-16 11:04:03 +0200

    chore: changed so that it starts with a capital letter

--- scenes/characters/chicken/brain_rl.gd
+++ scenes/characters/chicken/brain_rl.gd
@@ -2,12 +2,12 @@ class_name BrainRL extends Node
 
 var chicken: Chicken
 
-var last_state: String = "idle" # Saving the last action for the matrix
+var last_state: String = "Idle" # Saving the last action for the matrix
 
 # Set it to 1 because it should initially be 50%–50%
 var matrix = {
-	"idle": {"idle": 1, "harvest": 1},
-	"harvest": {"idle": 1, "harvest": 1}
+	"Idle": {"Idle": 1, "Harvest": 1},
+	"Harvest": {"Idle": 1, "Harvest": 1}
 }
 
 func setup(_chicken: Chicken) -> void:
@@ -16,11 +16,11 @@ func setup(_chicken: Chicken) -> void:
 func on_player_command() -> void:
 	var crop = chicken.find_ready_crop()
 	if crop:
-		matrix[last_state]["harvest"] += 1 # Incrementing the weight of the decision
+		matrix[last_state]["Harvest"] += 1 # Incrementing the weight of the decision
 		print("The chicken learnt")
 		
 		chicken.target_crop = crop
-		last_state = "harvest"
+		last_state = "Harvest"
 		chicken.state.transition_to("Movement")
 	else:
 		print("No crops ready to be harvested")
