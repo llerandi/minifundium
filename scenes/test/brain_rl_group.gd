0f309f71a38d63ecddd41270beb2761bfba293cf
Edward <llerandi@users.noreply.github.com>
2026-05-17 11:35:02 +0200

    feat: init the chicken and storing the last state

--- scenes/test/brain_rl_group.gd
+++ scenes/test/brain_rl_group.gd
@@ -1,1 +1,8 @@
-extends Node
+class_name BrainRLGroup extends Node
+
+var chicken: Chicken
+
+var last_state: String = "Idle" # Saving the last action for the matrix
+
+func setup(_chicken: Chicken) -> void:
+	chicken = _chicken
