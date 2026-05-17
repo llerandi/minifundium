2e1c0e31d2a46e92282818ab9b8a4fab4575fced
Edward <llerandi@users.noreply.github.com>
2026-05-17 11:36:57 +0200

    feat: commanded orders will write/read globally for the whole group

--- scenes/test/brain_rl_group.gd
+++ scenes/test/brain_rl_group.gd
@@ -6,3 +6,15 @@ var last_state: String = "Idle" # Saving the last action for the matrix
 
 func setup(_chicken: Chicken) -> void:
 	chicken = _chicken
+
+func on_player_command() -> void:
+	var crop = chicken.find_ready_crop()
+	if crop:
+		GlobalAi.matrix[last_state]["Harvest"] += 1 # Incrementing the weight of the decision
+		print("The chickens learnt, matrix: ", GlobalAi.matrix)
+		
+		chicken.target_crop = crop
+		last_state = "Harvest"
+		chicken.state.transition_to("Movement")
+	else:
+		print("No crops ready to be harvested")
